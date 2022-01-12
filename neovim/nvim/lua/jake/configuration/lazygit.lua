112790bcacf0ba227683e8d1b14eebadfaedda89
jakesjordan11 <jakesjordan11@gmail.com>
2022-01-12 01:43:38 -0700

    moved lazygit

--- neovim/nvim/lua/jake/configuration/lazygit.lua
+++ neovim/nvim/lua/jake/configuration/lazygit.lua
@@ -1,13 +1,13 @@
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
-function _G._lazygit_toggle()
+function Lazygit_toggle()
   lazygit:toggle()
 end
 
 vim.api.nvim_set_keymap(
   "n",
   "<leader>gg",
-  "<cmd> lua _G._lazygit_toggle()<cr>",
+  "<cmd> lua Lazygit_toggle()<cr>",
   { nowait = true, silent = true, noremap = true }
 )
