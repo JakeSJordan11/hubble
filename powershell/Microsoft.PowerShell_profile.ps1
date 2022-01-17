78b08670b26b3c49d51dc1a5448d01591ddd192d
jakesjordan11 <jakesjordan11@gmail.com>
2022-01-17 12:02:19 -0700

    powershell theme change and added l alias

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,8 +2,9 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module PSReadline
 
-# Set-Alias -Name l -Value Get-ChildItemColorFormatWide
+Set-Alias -Name l -Value Get-ChildItemColorFormatWide
 Set-Alias -Name vi -Value neovide
 Set-Alias -Name vim -Value neovide
 
 # Set-PoshPrompt -Theme jblab_2021
+Set-PoshPrompt -Theme zash
