9e7f4800c25a6f1919202459a9a7e200e6cfd9fc
jakesjordan11 <jakesjordan11@gmail.com>
2022-01-18 01:40:16 -0700

    cannonball

--- neovim/nvim/lua/jake/configuration/gitsigns.lua
+++ neovim/nvim/lua/jake/configuration/gitsigns.lua
@@ -14,3 +14,7 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+
+vim.api.nvim_set_keymap("n", "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", { nowait = true, silent = true, noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>Gitsigns toggle_signs<cr>", { nowait = true, silent = true, noremap = true })
